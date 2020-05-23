704bb44d000d18661f688f75124c80a09dcb1c4c
Gaelan D'costa <gdcosta@gmail.com>
2020-05-23 19:31:57 -0400

    Rip it up and start again

--- src/cooper/engine/dev.cljs
+++
@@ -1,8 +0,0 @@
-(ns cooper.engine.dev
-  (:require [cooper.engine.start]
-            [orchestra-cljs.spec.test :as st]
-            [expound.alpha :as expound]
-            [clojure.spec.alpha :as s]))
-
-(st/instrument)
-(set! s/*explain-out* expound/printer)
