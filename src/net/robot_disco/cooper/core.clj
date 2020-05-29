36f1db679c4dfc9c7d83d029ac9bc033e0c6f131
Gaelan D'costa <gdcosta@gmail.com>
2020-05-29 16:28:22 -0400

    Add docstrings to everything.

--- src/net/robot_disco/cooper/core.clj
+++ src/net/robot_disco/cooper/core.clj
@@ -1,20 +1,33 @@
 (ns net.robot-disco.cooper.core)
 
-(def ^:const MAX_SIZE 100)
-(def ^:const MAX_COUNTDOWN 10)
+(def
+  ^:const max-size
+  "Maximum size of petal"
+  100)
+(def ^:const max-countdown
+  "Maximum number of ticks petal can remain hidden for."
+  10)
 
 (defn make-petal
-  "rate - What percentage of the petal disappears with every tick?"
+  "Construct a petal. A `hidden` petal is one that is waiting to respawn; a player will die if they try to move here.  non-hidden petal is one the player can step on. The `rate` determines how quickly a visible petal will string or how long it will taken a hidden petal to respawn into a visible one."
   [rate hidden]
   (let [val (if hidden
-              MAX_COUNTDOWN
-              MAX_SIZE)]
+              max-countdown
+              max-size)]
     [val rate hidden]))
 
-(defn hidden? [[_ _ hidden]]
+(defn hidden?
+  "Return true if the petal is hidden, false if the petal is visible."
+  [[_ _ hidden]]
   hidden)
 
+(def visible?
+  "Return true if the petal is visible, false if the petal is hidden."
+  (complement #'hidden?))
+
 (defmulti tick
+  "Advance a `petal`, causing it to either shrink according to its specified rate
+  or to count down until the tick at which it respawns as a new visible petal."
   (fn [petal] (hidden? petal)))
 
 (defmethod tick true
