6cdd5c77c65cc9cdb7bc0da4a8860f42c494349f
Gaelan D'costa <gdcosta@gmail.com>
2020-05-30 19:24:36 -0400

    Really simple dev UI for watching petal mechanism.

--- src/net/robot_disco/cooper/core.clj
+++ src/net/robot_disco/cooper/core.clj
@@ -1,12 +1,21 @@
 (ns net.robot-disco.cooper.core
-  (:require [net.robot-disco.cooper.petal :as p]))
+  (:require [net.robot-disco.cooper.petal :as p]
+            [clojure.string :as str]))
 
 (defn my-loop []
-  (loop [x (take 2 (repeatedly (fn [] (p/make-hidden 10 (repeatedly #(+ 1 (rand-int 20))) (repeatedly #(+ 1 (rand-int 10)))))))]
-    (println (-> x first drop-last drop-last) " " (-> x second drop-last drop-last))
+  (loop [game-board (take 32 (repeatedly (fn [] (p/make-hidden 10 (repeatedly #(+ 1 (rand-int 20))) (repeatedly #(+ 1 (rand-int 10)))))))]
+    (loop [row (take 8 game-board)
+           rest (drop 8 game-board)]
+      (let [string (map #(format "%03d%s" (get % 1) (if (p/hidden? %) "h" " ")) row)
+            string (str/join " " string)]
+        (println string)
+        (if (seq rest)
+          (recur (take 8 rest)
+                 (drop 8 rest))
+          (newline))))
     (if (= (read-line) "q")
       nil
-      (recur (map p/tick x)))))
+      (recur (map p/tick game-board)))))
 
 (comment
   (require 'net.robot-disco.cooper.core :reload-all)
