5bec74f86fc0cd8e9f8b2a7b33c27b03950e0460
Gaelan D'costa <gdcosta@gmail.com>
2020-06-14 00:09:04 -0400

    Apparently test expectation ordering is = actual expected

--- test/net/robot_disco/cooper/petal_test.clj
+++ test/net/robot_disco/cooper/petal_test.clj
@@ -6,24 +6,24 @@
   (let [old (sut/make-petal false 10 1 '() '())
         new (sut/advance old)]
     (is (< (sut/countdown new) (sut/countdown old)))
-    (is (= (sut/hidden? old) (sut/hidden? new)))))
+    (is (= (sut/hidden? new) (sut/hidden? old)))))
 
 (deftest petal-flip-visible-to-hidden
   (let [old (sut/make-petal false 0 1 (iterate inc 2) (iterate inc 3))
         new (sut/advance old)]
     (is (sut/hidden? new))
     (is (= sut/max-countdown (sut/countdown new)))
-    (is (= (-> old sut/hidden-rates first)
-           (sut/current-rate new)))
-    (is (= (-> old sut/hidden-rates (nth 1))
-           (-> new sut/hidden-rates first)))))
+    (is (= (sut/current-rate new)
+           (-> old sut/hidden-rates first)))
+    (is (= (-> new sut/hidden-rates first)
+           (-> old sut/hidden-rates (nth 1))))))
 
 (deftest petal-flip-hidden-to-visible
   (let [old (sut/make-petal true 0 1 (iterate inc 2) (iterate inc 3))
         new (sut/advance old)]
     (is (sut/visible? new))
-    (is (= sut/max-countdown (sut/countdown new)))
-    (is (= (-> old sut/visible-rates first)
-           (sut/current-rate new)))
-    (is (= (-> old sut/visible-rates (nth 1))
-           (-> new sut/visible-rates first)))))
+    (is (= (sut/countdown new) sut/max-countdown))
+    (is (= (sut/current-rate new)
+           (-> old sut/visible-rates first)))
+    (is (= (-> new sut/visible-rates first)
+           (-> old sut/visible-rates (nth 1))))))
--- test/net/robot_disco/cooper/player_test.clj
+++ test/net/robot_disco/cooper/player_test.clj
@@ -6,47 +6,47 @@
 (deftest player-move-left
   (let [old {:row 1 :col 1}
         new (sut/move-left old)]
-    (is (= (-> old sut/col dec) (sut/col new)))
-    (is (= (sut/row old) (sut/row new)))))
+    (is (= (sut/col new) (-> old sut/col dec)))
+    (is (= (sut/row new) (sut/row old)))))
 
 (deftest player-on-edge-move-left
   (let [old {:row 1 :col c/min-row}
         new (sut/move-left old)]
-    (is (= (sut/col old) (sut/col new)))
-    (is (= (sut/row old) (sut/row new)))))
+    (is (= (sut/col new) (sut/col old)))
+    (is (= (sut/row new) (sut/row old)))))
 
 (deftest player-move-right
   (let [old {:row 1 :col 1}
         new (sut/move-right old)]
-    (is (= (-> old sut/col inc) (sut/col new)))
-    (is (= (sut/row old) (sut/row new)))))
+    (is (= (sut/col new) (-> old sut/col inc)))
+    (is (= (sut/row new) (sut/row old)))))
 
 (deftest player-on-edge-move-right
   (let [old {:row 1 :col c/max-col}
         new (sut/move-right old)]
-    (is (= (sut/col old) (sut/col new)))
-    (is (= (sut/row old) (sut/row new)))))
+    (is (= (sut/col new) (sut/col old)))
+    (is (= (sut/row new) (sut/row old)))))
 
 (deftest player-move-up
   (let [old {:row 1 :col 1}
         new (sut/move-up old)]
-    (is (= (-> old sut/row inc) (sut/row new)))
-    (is (= (sut/col old) (sut/col new)))))
+    (is (= (sut/row new) (-> old sut/row inc)))
+    (is (= (sut/col new) (sut/col old)))))
 
 (deftest player-on-edge-move-up
   (let [old {:row c/max-row :col 1}
         new (sut/move-up old)]
-    (is (= (sut/row old) (sut/row new)))
-    (is (= (sut/col old) (sut/col new)))))
+    (is (= (sut/row new) (sut/row old)))
+    (is (= (sut/col new) (sut/col old)))))
 
 (deftest player-move-down
   (let [old {:row 1 :col 1}
         new (sut/move-down old)]
-    (is (= (-> old sut/row dec) (sut/row new)))
-    (is (= (sut/col old) (sut/col new)))))
+    (is (= (sut/row new) (-> old sut/row dec)))
+    (is (= (sut/col new) (sut/col old)))))
 
 (deftest player-on-edge-move-down
   (let [old {:row c/min-row :col 1}
         new (sut/move-down old)]
-    (is (= (sut/row old) (sut/row new)))
-    (is (= (sut/col old) (sut/col new)))))
+    (is (= (sut/row new) (sut/row old)))
+    (is (= (sut/col new) (sut/col old)))))
