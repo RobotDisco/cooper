bee0d2dac744bb292687a23d4a1302decd3daaea
Gaelan D'costa <gdcosta@gmail.com>
2022-12-26 20:01:26 -0500

    Switch to nix flakes

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Cooper memorial game";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+      {
+        devShells."${system}".default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            clojure
+            git
+          ];
+        };
+      };
+}
