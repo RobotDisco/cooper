129d70646f6a8ec634d32570db8241d69f4963f6
Gaelan D'costa <gdcosta@gmail.com>
2024-07-20 11:52:04 -0400

    Render mock game board in simple ugly way.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,72 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text)
+import Html exposing (Html, div, span, text)
 
-main = Browser.sandbox { init = 0, update = update, view = view }
-                             
-update : Int -> Int -> Int
-update _ _ = 0
 
-view : Int -> Html msg
-view _ = text "Hello"
+type alias Model =
+    { rows : Int
+    , cols : Int
+
+    -- It'd be nice if I could derive dimensions from the content
+    -- not additional metadata.
+    , circles : List (List Int)
+    }
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+init : Model
+init =
+    let
+        rows =
+            4
+
+        cols =
+            7
+    in
+    { rows = rows
+    , cols = cols
+
+    -- For now, generate fully extended petals to start.
+    , circles =
+        List.map
+            (\_ -> List.map (\_ -> 100) (List.range 1 cols))
+            (List.range 1 rows)
+    }
+
+
+update : Model -> Model -> Model
+update state _ =
+    state
+
+
+view : Model -> Html Model
+view model =
+    -- Board div
+    div []
+        -- Render each row. This probably should be its own function for
+        -- readability.
+        (List.map
+            (\row ->
+                div []
+                    -- Render each column
+                    (List.map
+                        (\col ->
+                            span []
+                                -- Pad each value by a space on each side
+                                [ text " "
+                                , text
+                                    (String.fromInt
+                                        col
+                                    )
+                                , text " "
+                                ]
+                        )
+                        row
+                    )
+            )
+            model.circles
+        )
